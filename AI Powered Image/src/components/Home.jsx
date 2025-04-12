bee49a7d6e7eb46da427eb9d82eee8fc07803d6e
Pooja <poojasawant1030@gmail.com>
2025-04-12 12:54:26 +0530

    main page ui implementation

---
+++ AI Powered Image/src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ImageUpload from './ImageUpload'
+import ImagePreview from './ImagePreview'
+
+const Home = () => {
+  return (
+    <div className='w-full'>
+      <ImageUpload />
+      <ImagePreview />
+    </div>
+  )
+}
+
+export default Home
